450aa17e037ac1bec268d802ea179b0376af0c69
taylorchu <tailinchu@gmail.com>
2013-05-30 22:04:23 -0700

    first commit

---
+++ baker.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+source config.sh
+source library.sh
+
+case $1 in
+    bake)
+        echo "baking blog posts..."
+        rm -rf $OUTPUT_DIR/{$STYLESHEET_DIR,$IMAGE_DIR,*.html}
+        cp -r "$THEME_DIR/$STYLESHEET_DIR" "$OUTPUT_DIR"; cp -r "$THEME_DIR/$IMAGE_DIR" "$OUTPUT_DIR"
+        declare -A indexVars=([SITE_NAME]="$SITE_NAME" [AUTHOR]="$AUTHOR" [title]="home")
+        i=0
+        for src in $(ls "$POST_DIR" | grep "\.md$" | sort -r); do
+            dest="$(basename $src $POST_EXT)$OUTPUT_EXT"
+            echo " (*) $POST_DIR/$src -> $OUTPUT_DIR/$dest"
+            layout="$(header "$POST_DIR/$src" layout)"
+            title="$(header "$POST_DIR/$src" title)"
+            date="$(header "$POST_DIR/$src" date)"
+            content="$(body "$POST_DIR/$src" | ./Markdown.pl)"
+            declare -A vars=([title]="$title" [date]="$date" [content]="$content" [link]="$dest")
+            vars[SITE_NAME]="$SITE_NAME"
+            vars[AUTHOR]="$AUTHOR"
+            indexVars[post.${i}]="$(declare -p vars)"
+            doLayout "$layout" "$(declare -p vars)" | sed '/^[ ]*$/d' > "$OUTPUT_DIR/$dest"
+            (( i++ ))
+        done
+        echo " (*) creating index"
+        doLayout "index" "$(declare -p indexVars)" | sed '/^[ ]*$/d' > "$OUTPUT_DIR/index$OUTPUT_EXT"
+    ;;
+    new)
+        if [[ -z "${@:2}" ]]; then
+            echo "we need a title"
+            exit 1
+        fi
+
+        title="$(date +%Y-%m-%d)-$(slug ${@:2})"
+        cat > "$POST_DIR/$title$POST_EXT" << EOF
+---
+layout: post
+title: ${@:2}
+date: $(date +"%Y-%m-%d %H:%M")
+categories:
+---
+EOF
+echo "$title is ready in '$POST_DIR'"
+    ;;
+    *)
+        cat <<EOF
+baker
+    new [new post title]    create new post
+    bake                    ship static page
+EOF
+    ;;
+esac
---
+++ config.sh
@@ -0,0 +1,15 @@
+# def
+readonly OUTPUT_DIR=bakery
+readonly OUTPUT_EXT=.html
+readonly POST_DIR=posts
+readonly POST_EXT=.md
+readonly THEME_DIR=themes
+readonly LAYOUT_DIR=layouts
+readonly LAYOUT_EXT=.html
+readonly INCLUDE_DIR=includes
+readonly INCLUDE_EXT=.html
+readonly STYLESHEET_DIR=stylesheets
+readonly IMAGE_DIR=images
+
+readonly SITE_NAME="the denorm guy"
+readonly AUTHOR="tai"
